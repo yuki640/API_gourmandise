9f367630e1e8877ec0c7d5565843fcefcc2a6497
ThomasP <thomas.pareschi@campus-centre.fr>
2023-09-11 11:55:26 +0200

    Travail a moi

--- endpoints.js
+++ endpoints.js
@@ -1,65 +1,61 @@
 module.exports = function (app, pool, bcrypt, jwt) {
+  // GET all products
+  app.get("/products", async (req, res) => {
+    try {
+      const [rows] = await pool.execute("SELECT * FROM produit");
+      res.status(200).json(rows);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
-    // get all produit
-    app.get("/produit", async (req, res) => {
-        try{
-            const [rows] = await pool.execute("SELECT * FROM produit");
-            res.status(200).json(rows);
-        } catch (err){
-            res.status(500).json({message : err.message})
-        }
-    })
+  //GET all order
+  app.get("/order", async (req, res) => {
+    try {
+      const [rows] = await pool.execute("SELECT * FROM commande");
+      res.status(200).json(rows);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
-     // get all client
-     app.get("/client", async (req, res) => {
-        try{
-            const [rows] = await pool.execute("SELECT * FROM client");
-            res.status(200).json(rows);
-        } catch (err){
-            res.status(500).json({message : err.message})
-        }
-    })
+  //GET all order_ligne
+  app.get("/order_ligne", async (req, res) => {
+    try {
+      const [rows] = await pool.execute("SELECT * FROM ligne_commande");
+      res.status(200).json(rows);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
+  //GET all client
+  app.get("/clients", async (req, res) => {
+    try {
+      const [rows] = await pool.execute("SELECT * FROM client");
+      res.status(200).json(rows);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  });
 
-
-     // get all commande
-     app.get("/commande", async (req, res) => {
-        try{
-            const [rows] = await pool.execute("SELECT * FROM commande");
-            res.status(200).json(rows);
-        } catch (err){
-            res.status(500).json({message : err.message})
-        }
-    })
-
-
-    // get all ligne_commande
-    app.get("/ligne_commande", async (req, res) => {
-        try{
-            const [rows] = await pool.execute("SELECT * FROM ligne_commande");
-            res.status(200).json(rows);
-        } catch (err){
-            res.status(500).json({message : err.message})
-        }
-    })
-
-    //Register
-    app.post("/regestier", async (req, res) => {
-        const {nom, adresse, cp, ville, telephone, motdepasse, email} = req.body;
-        const hashedPassword = await bcrypt.hash(motdepasse, 10);
-        const values = [mon, adresse, cp, ville, telephone, hashedPassword, email];
-        try {
-            await pool.execute(
-                "INSERT INTO client (nom, adresse, cp, ville, telephone, motdepasse, email) VALUES (?,?,?,?,?,?,?)",
-                 values
-                 );
-                res.sendStatus(201);
-        } catch (err){
-            console.log(err);
-            res.json({
-                succes: false,
-                message: "Une erreur est survenue lors de l'enregistrement.",
-            });
-        }
-    });
-}
+  // Register
+  app.post("/register", async (req, res) => {
+    const { nom, adresse, cp, ville, telephone, email } = req.body;
+    const hashedPassword = await bcrypt.hash(motdepasse, 10);
+    const values = [nom, adresse, cp, ville, telephone, email];
+    try {
+      await pool.execute(
+        "INSERT INTO client (nom, adresse, cp, ville, telephone, motdepasse, email) VALUES (?,?,?,?,?,?,?)",
+        values
+      );
+      res.sendStatus(201);
+    } catch (err) {
+      console.log(err);
+      res.json({
+        success: false,
+        message: "Une erreur est survenue lors de l'enregistrement",
+      });
+    }
+  });
+};
